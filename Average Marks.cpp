ceb0c06b109f89c2803cf6f63394370d14c84822
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-29 12:11:32 +0530

    Add files via upload

---
+++ Average Marks.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+#include <utility>
+
+pair<char, int> averageMarks(char firstLetterOfName, int m1, int m2, int m3) {
+    // Write your code here.
+    int avg ;
+    avg = (m1+m2+m3)/3;
+    pair<char,int> p;
+    p.first=firstLetterOfName;
+    p.second=avg;
+    return p;
+}
