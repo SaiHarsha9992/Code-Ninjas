2077952d7d7e7eb1c63eaffaa09d3d9892fbaf7e
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-05-07 19:06:17 +0530

    Add files via upload

---
+++ Find Area of Rectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+		Scanner s = new Scanner (System.in);
+		int l = s.nextInt();
+		int b = s.nextInt();
+		int a = l*b;
+		System.out.println(a);
+	}
+}
