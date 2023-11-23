3981ad1ed0bccfece44d112a65492afeefa0a68c
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-11-23 22:14:33 +0530

    Add files via upload

---
+++ Factorial of a Large Number.py
@@ -0,0 +1,10 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def calculateFactorial(n):
+    #    Write your code here.
+    pass
+    s = factorial(n)
+    return str(s) 
